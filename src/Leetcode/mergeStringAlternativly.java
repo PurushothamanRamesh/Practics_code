9b8aa295c4b8703326cedd9adfd9b61714f763bf
purushothaman r <purushothamanr011@gmail.com>
2024-05-09 19:40:54 +0530

    oct 13

---
+++ src/Leetcode/mergeStringAlternativly.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+public class mergeStringAlternativly {
+    public static void main(String[] args) {
+        String str1="pqrd";
+        String str2="QUny";
+       String answer= mergeStringAlternate(str1,str2);
+        System.out.println(answer);
+    }
+
+    private static String mergeStringAlternate(String str1, String str2) {
+        return null;
+    }
+}
--- src/zohoquestions/codingquestion3.java
+++ src/zohoquestions/codingquestion3.java
@@ -1,3 +1,5 @@
+package zohoquestions;
+
 public class codingquestion3 {
     public static void main(String[] args) {
         int[] nums={4,1,-1,0,0,1,2,-1,4};
